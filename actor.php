1ed0504780237b9c64815f4a59549c21270e1c1c
khzheng24998 <kennyzheng24998@gmail.com>
2019-02-02 22:06:53 -0800

    Added Actor pages

--- actor.php
+++ actor.php
@@ -16,5 +16,114 @@
       </div>
 
     </div>
+
+    <div id="page-body" style="padding: 5px 0px 15px 15px;">
+      <?php fetch(); ?>
+    </div>
+
   </body>
 </html>
+
+<?php
+function printBio($tuple) {
+
+  print "<strong>Name: </strong><p style='display: inline-block; margin: 2;'>$tuple[0]</p><br>";
+  print "<strong>Sex: </strong><p style='display: inline-block; margin: 2;'>$tuple[1]</p><br>";
+  print "<strong>Born: </strong><p style='display: inline-block; margin: 2;'>$tuple[2]</p><br>";
+
+  if ($tuple[3])
+    print "<strong>Died: </strong><p style='display: inline-block; margin: 2;'>$tuple[3]</p><br>";
+}
+
+function printResults($attrs, $tuples, $rel) {
+  //Query returned no tuples
+  if (count($tuples) == 0) {
+    print "<p>No results found.</p>";
+    return;
+  }
+
+  //Format query results and display them in HTML table
+  print '<table border="1" cellspacing="1" cellpadding="2">';
+
+  print '<tr align="center">';
+  for ($i = 0; $i < count($attrs); $i++) {
+    if ($i == 0)
+      continue;
+    print "<th>$attrs[$i]</th>";
+  }
+  print "</tr>";
+
+  foreach ($tuples as $tuple) {
+    print '<tr align="center">';
+    for ($i = 0; $i < count($tuple); $i++) {
+      if ($i == 0)
+        continue;
+      if ($i == 1)
+        print "<td><a href='$rel.php?id=$tuple[0]'>$tuple[$i]<a></td>";
+      else
+        print "<td>$tuple[$i]</td>";
+    }
+    print "</tr>";
+  }
+  print "</table>";
+}
+
+function issueQuery($query, $db_connection, &$rows, &$cols) {
+  //Issue query to database
+  $rs = mysql_query($query, $db_connection);
+  if (!$rs) {
+    $errmsg = mysql_error($rs);
+    print "Query issue failed. $errmsg";
+    exit(1);
+  }
+
+  //Retrieve query results and store them in $rows
+  $rows = [];
+  while ($row = mysql_fetch_row($rs))
+    array_push($rows, $row);
+
+  //Retrieve column names and store them in $cols
+  $cols = [];
+  for ($i = 0; $i < mysql_num_fields($rs); $i++) {
+    $meta = mysql_fetch_field($rs, $i);
+    array_push($cols, $meta->name);
+  }
+}
+
+function fetch()
+{
+  //Establish connection to MySQL database
+  $db_connection = mysql_connect("localhost", "cs143", "");
+  if (!$db_connection) {
+    $errmsg = mysql_error($db_connection);
+    print "Connection failed. $errmsg";
+    exit(1);
+  }
+
+  //Select database to query
+  $db = "CS143";
+  mysql_select_db($db, $db_connection);
+
+  //Format query using user input
+  $id = $_GET["id"];
+
+  $rows = [];
+  $cols = [];
+  $query = "SELECT CONCAT(first, ' ', last) AS Name, sex AS Sex, dob AS DOB, dod AS DOD FROM Actor WHERE id = $id;";
+  issueQuery($query, $db_connection, $rows, $cols);
+
+  print '<h3>Bio</h3>';
+  printBio($rows[0]);
+
+  $rows = [];
+  $cols = [];
+  $query = "SELECT mid, title AS Title, year AS Year, role AS Role FROM MovieActor JOIN Movie ON MovieActor.mid = Movie.id WHERE aid = $id;";
+  issueQuery($query, $db_connection, $rows, $cols);
+
+  print '<h3>Filmography</h3>';
+  printResults($cols, $rows, "movie");
+
+  //Close database connection
+  mysql_close($db_connection);
+}
+?>
--- search.php
+++ search.php
@@ -17,7 +17,7 @@
 
     </div>
 
-    <div id="results" style="padding: 15px 0px 15px 15px;">
+    <div id="results" style="padding: 5px 0px 15px 15px;">
       <p>Showing results for "<?php print $_GET['keyword'] ?>"</p>
       <?php fetch(); ?>
     </div>
@@ -26,95 +26,99 @@
 </html>
 
 <?php
+function printResults($attrs, $tuples, $rel) {
+  //Query returned no tuples
+  if (count($tuples) == 0) {
+    print "<p>No results found.</p>";
+    return;
+  }
 
-  /* Helper functions */
+  //Format query results and display them in HTML table
+  print '<table border="1" cellspacing="1" cellpadding="2">';
 
-  function printResults($attrs, $tuples, $rel) {
-    //Query returned no tuples
-    if (count($tuples) == 0) {
-      print "<p>No results found.</p>";
-      return;
-    }
+  print '<tr align="center">';
+  for ($i = 0; $i < count($attrs); $i++) {
+    if ($i == 0)
+      continue;
+    print "<th>$attrs[$i]</th>";
+  }
+  print "</tr>";
 
-    //Format query results and display them in HTML table
-    print '<table border="1" cellspacing="1" cellpadding="2">';
+  foreach ($tuples as $tuple) {
     print '<tr align="center">';
-    foreach ($attrs as $attr)
-      print "<th>$attr</th>";
+    for ($i = 0; $i < count($tuple); $i++) {
+      if ($i == 0)
+        continue;
+      if ($i == 1)
+        print "<td><a href='$rel.php?id=$tuple[0]'>$tuple[$i]<a></td>";
+      else
+        print "<td>$tuple[$i]</td>";
+    }
     print "</tr>";
-    foreach ($tuples as $tuple) {
-      print '<tr align="center">';
-      for ($i = 0; $i < count($tuple); $i++) {
-        if ($i == 0)
-          print "<td><a href='$rel.php?name=$tuple[$i]'>$tuple[$i]<a></td>";
-        else
-          print "<td>$tuple[$i]</td>";
-      }
-      print "</tr>";
-    }
-    print "</table>";
+  }
+  print "</table>";
+}
+
+function issueQuery($query, $db_connection, &$rows, &$cols) {
+  //Issue query to database
+  $rs = mysql_query($query, $db_connection);
+  if (!$rs) {
+    $errmsg = mysql_error($rs);
+    print "Query issue failed. $errmsg";
+    exit(1);
   }
 
-  function issueQuery($query, $db_connection, &$rows, &$cols) {
-    //Issue query to database
-    $rs = mysql_query($query, $db_connection);
-    if (!$rs) {
-      $errmsg = mysql_error($rs);
-      print "Query issue failed. $errmsg";
-      exit(1);
-    }
+  //Retrieve query results and store them in $rows
+  $rows = [];
+  while ($row = mysql_fetch_row($rs))
+    array_push($rows, $row);
 
-    //Retrieve query results and store them in $rows
-    $rows = [];
-    while ($row = mysql_fetch_row($rs))
-      array_push($rows, $row);
+  //Retrieve column names and store them in $cols
+  $cols = [];
+  for ($i = 0; $i < mysql_num_fields($rs); $i++) {
+    $meta = mysql_fetch_field($rs, $i);
+    array_push($cols, $meta->name);
+  }
+}
 
-    //Retrieve column names and store them in $cols
-    $cols = [];
-    for ($i = 0; $i < mysql_num_fields($rs); $i++) {
-      $meta = mysql_fetch_field($rs, $i);
-      array_push($cols, $meta->name);
-    }
+function fetch()
+{
+  //Establish connection to MySQL database
+  $db_connection = mysql_connect("localhost", "cs143", "");
+  if (!$db_connection) {
+    $errmsg = mysql_error($db_connection);
+    print "Connection failed. $errmsg";
+    exit(1);
   }
 
-  function fetch()
-  {
-    //Establish connection to MySQL database
-    $db_connection = mysql_connect("localhost", "cs143", "");
-    if (!$db_connection) {
-      $errmsg = mysql_error($db_connection);
-      print "Connection failed. $errmsg";
-      exit(1);
-    }
+  //Select database to query
+  $db = "CS143";
+  mysql_select_db($db, $db_connection);
 
-    //Select database to query
-    $db = "CS143";
-    mysql_select_db($db, $db_connection);
-
-    //Format query using user input
-    $keyword = $_GET["keyword"];
-    if ($keyword == "") {
-      print "<p style='color: red;'>Please enter a keyword to search.</p>";
-      return;
-    }
-
-    $rows = [];
-    $cols = [];
-    $query = "SELECT CONCAT(first, ' ', last) AS Name, sex AS Sex, dob AS DOB, dod AS DOD FROM Actor WHERE CONCAT(first, ' ', last) LIKE '%$keyword%' OR CONCAT(last, ' ', first) LIKE '%$keyword%';";
-    issueQuery($query, $db_connection, $rows, $cols);
-
-    print '<h3>Actors/Actresses</h3>';
-    printResults($cols, $rows, "actor");
-
-    $rows = [];
-    $cols = [];
-    $query = "SELECT title AS Title, year AS Year, rating AS Rating, company AS Company FROM Movie WHERE title LIKE '%$keyword%';";
-    issueQuery($query, $db_connection, $rows, $cols);
-
-    print '<h3>Movies</h3>';
-    printResults($cols, $rows, "movie");
-
-    //Close database connection
-    mysql_close($db_connection);
+  //Format query using user input
+  $keyword = $_GET["keyword"];
+  if ($keyword == "") {
+    print "<p style='color: red;'>Please enter a keyword to search.</p>";
+    return;
   }
+
+  $rows = [];
+  $cols = [];
+  $query = "SELECT id, CONCAT(first, ' ', last) AS Name, sex AS Sex, dob AS DOB, dod AS DOD FROM Actor WHERE CONCAT(first, ' ', last) LIKE '%$keyword%' OR CONCAT(last, ' ', first) LIKE '%$keyword%';";
+  issueQuery($query, $db_connection, $rows, $cols);
+
+  print '<h3>Actors/Actresses</h3>';
+  printResults($cols, $rows, "actor");
+
+  $rows = [];
+  $cols = [];
+  $query = "SELECT id, title AS Title, year AS Year, rating AS Rating, company AS Company FROM Movie WHERE title LIKE '%$keyword%';";
+  issueQuery($query, $db_connection, $rows, $cols);
+
+  print '<h3>Movies</h3>';
+  printResults($cols, $rows, "movie");
+
+  //Close database connection
+  mysql_close($db_connection);
+}
 ?>
